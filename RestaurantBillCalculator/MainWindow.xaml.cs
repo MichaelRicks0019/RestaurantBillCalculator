d8825a30eeb1ad7f7cf4683330dc8f744659c086
Michael Ricks <michaelricks0019@gmail.com>
2022-05-18 11:49:03 -0600

    Add project files.

---
+++ RestaurantBillCalculator/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantBillCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Beverage
+    {
+        private string Category { get; set; }
+        private string Name { get; set; }
+        private decimal Price { get; set; }
+
+        public Beverage (string category, string name, decimal price)
+        {
+            Category = category;
+            Name = name;
+            Price = price;
+        }
+
+        public string GetNameAndPrice()
+        {
+            return $"{Name}   ${Price}";
+        }
+
+        public string GetCategory ()
+        {
+            return Category;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+
+    }
+
+}
